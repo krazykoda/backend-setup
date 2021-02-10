42db1d6fe31c08c8cbbf04314a5592ade2268ca8
johnson <kryzbone@gmail.com>
2021-02-10 14:34:46 +0000

    added error handle to express

--- express.js
+++ express.js
@@ -14,5 +14,17 @@ app.get("/login", handler("<h1>Login Page</h1>"))
 app.get("/register", handler("<h1>Register Page</h1>"))
 app.get("/contact", handler("<h1>No Contact List</h1>"))
 
+// 404 handle
+app.use((req, res, next) => {
+    const err = Error("Page Not found")
+    err.status = 404
+    next(err)
+})
+
+//error handler
+app.use((err, req, res, next) => {
+    const statusCode = err.status || 500
+    res.status(statusCode).send(`<h2>${err.message}</h1>`)
+})
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
